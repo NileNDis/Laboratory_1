8670a758ae0202f88c78f3a618660ee2048f931a
NileNDis <denis.gg2018@gmail.net>
2022-02-04 13:38:12 +0200

    5

--- Laboratory_1.cpp
+++ Laboratory_1.cpp
@@ -5,7 +5,7 @@ int main()
     setlocale(LC_ALL, "rus");
     std::cout << "Hello World!\n";
     std::cout << "ASD\n";
-    std::cout << "Обновлено 3\n";// не верно отображается в гитхабе((
+    std::cout << "Обновлено 3\n";// not displayed correctly on github ((
     std::cout << "Update 4\n";
     system("PAUSE");
 }
